dba46cb7410fb301899f94632a8440fb09eda12f
greyhat3 <joelbaffour8@gmail.com>
2018-09-27 11:56:01 +0100

    set up views and urls

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services')
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #return render('',{})
-    return HttpResponse('bbibmaeoimfdbmeionamfebrmrborimim')
+    return HttpResponse('bbibmaeoimfdbmeionamfebrmrb orimim')
+
+
+def about(request):
+    return HttpResponse('bmeionamfebrmrb orimim')
+
+def services(request):
+    return HttpResponse('bbibmaeoimfdbmeionamjkgk,kgubhoyi8hfebrmrb orimim')
